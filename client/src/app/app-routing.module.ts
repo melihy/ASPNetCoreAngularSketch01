a3e5270b9a3c1b34965b746ff2f9e8a80bfafaf4
melih <melihayhan@gmail.com>
2023-11-15 00:44:08 +0300

    member editing checkpoint #1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { authGuard } from './_guard/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,6 +20,7 @@ const routes: Routes = [
     children: [
       { path: 'members', component: MemberListComponent },
       { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/:edit', component: MemberEditComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent }
     ]
